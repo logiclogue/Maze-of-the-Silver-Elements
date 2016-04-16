cfaef84a7b83c29c7aafb177b6f5b5050e734bef
logiclogue <jordanpaullord@hotmail.co.uk>
2016-04-16 07:25:36 +0100

    Configure gulp

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var watch = require('gulp-watch');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+var paths = {
+    javascript: [
+        'js/Main.js',
+        'js/*.js'
+    ],
+    css: 'css/*.css'
+}
+
+
+// JavaScript
+gulp.task('javascript', function () {
+    return browserify(paths.javascript[0])
+        .transform('babelify', {
+            presets: ['es2015'],
+            sourceMaps: false
+        })
+        .bundle()
+        .on('error', function (error) {
+            console.log(error);
+            this.emit('end');
+        })
+        .pipe(source('all.js'))
+        .pipe(gulp.dest('build'));
+});
+
+// CSS
+gulp.task('css', function () {
+    return gulp.src(paths.css)
+        .pipe(concat('all.css'))
+        .pipe(gulp.dest('build'));
+});
+
+
+// Watches all
+gulp.task('watch', function () {
+    gulp.watch([paths.javascript, paths.css], ['javascript', 'css']);
+});
+
+
+// Default task
+gulp.task('default', ['javascript', 'css', 'watch']);
